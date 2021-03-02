8b0c69885321b9a8378ec313de12b230b7700707
tudtude <tudtude@gmail.com>
2021-03-02 22:05:06 +0700

    Edit script to support docker 5:19

--- prepare.sh
+++ prepare.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 echo "------------------ Install basic require ------------------"
-apt-get install -y software-properties-common apparmor-utils apt-transport-https ca-certificates dbus jq network-manager htop
+apt-get install -y  software-properties-common apparmor-utils apt-transport-https ca-certificates dbus jq network-manager gnupg2 htop
+
+echo "------------------ Install docker ------------------"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt install -y docker-ce-cli=5:19.03.14~3-0~debian-buster && apt install docker-ce=5:19.03.14~3-0~debian-buster
+
+echo "------------------ Remove unused ------------------"
 systemctl disable ModemManager
 systemctl stop ModemManager
 
-echo "------------------ Install docker ------------------"
-apt install docker-ce=5:19.03.13~3-0~debian-buster
+echo "------------------ Install Home Assistant ------------------"
+curl -sL "https://raw.githubusercontent.com/Kanga-Who/home-assistant/master/supervised-installer.sh" | bash -s -- -m raspberrypi4-64
 
-
-
-# echo "------------------ Install Home Assistant ------------------"
-# curl -sL "https://raw.githubusercontent.com/Kanga-Who/home-assistant/master/supervised-installer.sh" | bash -s -- -m raspberrypi4-64
-
-# echo "------------------ Clear unused file ------------------"
-# apt update && sudo apt upgrade -y && sudo apt autoremove –y
+echo "------------------ Clear unused file ------------------"
+apt update && sudo apt upgrade -y && sudo apt autoremove –y
 
